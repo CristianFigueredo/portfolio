78107e2bdc795d27b32cb8671d537f7f78f7605a
Cristian Figueredo <cristian28figueredo@gmail.com>
2023-07-30 15:47:42 -0400

    build: add prettier-plugin-tailwindcss + configuration

---
+++ prettier.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Options} */
+module.exports = {
+   plugins: ['prettier-plugin-tailwindcss'],
+   trailingComma: 'es5',
+   semi: false,
+   singleQuote: true,
+   tailwindConfig: './tailwind.config.js',
+ }
