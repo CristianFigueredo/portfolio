3efecfb72e517e4c8b93ca46630182ff219ec266
Cristian Figueredo <cristian28figueredo@gmail.com>
2023-09-02 18:18:02 -0400

    feat: remove tailwind

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import("prettier").Options} */
 module.exports = {
-   plugins: ['prettier-plugin-tailwindcss'],
    trailingComma: 'es5',
    semi: false,
    singleQuote: true,
-   tailwindConfig: './tailwind.config.js',
  }
