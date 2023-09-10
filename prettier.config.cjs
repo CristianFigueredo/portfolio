846a4f0eebb971af1e9a0ae98fc58efcddd900fd
Cristian Figueredo <cristian28figueredo@gmail.com>
2023-09-10 01:42:41 -0400

    style: format all files with prettier

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,4 +3,13 @@ module.exports = {
   trailingComma: 'es5',
   semi: false,
   singleQuote: true,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 }
