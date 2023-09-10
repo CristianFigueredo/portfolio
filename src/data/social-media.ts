70f0077f1713747eb0eccc307415f10f787193a8
Cristian Figueredo <cristian28figueredo@gmail.com>
2023-09-10 01:26:25 -0400

    build: add prettier-plugin-astro

--- src/data/social-media.ts
+++ src/data/social-media.ts
@@ -1,6 +1,6 @@
-const LINKEDIN_URL = "https://www.linkedin.com/in/cristian-figueredo" 
-const GITHUB_URL = "https://github.com/CristianFigueredo"
-const TWITTER_URL = "https://x.com/_CrisFigueredo"
+const LINKEDIN_URL = 'https://www.linkedin.com/in/cristian-figueredo'
+const GITHUB_URL = 'https://github.com/CristianFigueredo'
+const TWITTER_URL = 'https://x.com/_CrisFigueredo'
 
 export const PROFILE_URL = {
   LINKEDIN: LINKEDIN_URL,
