86d6ce4a8c86dd187dc98e664ad298962bc0cefc
Cristian Figueredo <cristian28figueredo@gmail.com>
2023-09-10 01:17:24 -0400

    feat: functional social media buttons + animations

---
+++ src/data/social-media.ts
@@ -0,0 +1,9 @@
+const LINKEDIN_URL = "https://www.linkedin.com/in/cristian-figueredo" 
+const GITHUB_URL = "https://github.com/CristianFigueredo"
+const TWITTER_URL = "https://x.com/_CrisFigueredo"
+
+export const PROFILE_URL = {
+  LINKEDIN: LINKEDIN_URL,
+  GITHUB: GITHUB_URL,
+  TWITTER: TWITTER_URL,
+}
