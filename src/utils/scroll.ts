203493faf5031d0d0deeafaf5dac9fc6c3dca62a
Cristian Figueredo <cristian28figueredo@gmail.com>
2023-09-14 23:25:46 -0400

    style: format 'src/utils/scroll' code

--- src/utils/scroll.ts
+++ src/utils/scroll.ts
@@ -5,9 +5,9 @@
  * @example getPercentageScrolledFromTop() === 100 // when the user scrolls to the end of the page
  *
  * @see https://www.geeksforgeeks.org/offsetwidth-clientwidth-scrollwidth-and-height-respectively-in-css/
- **/
+ */
 export function getPercentageScrolledFromTop(): number {
-  const { scrollTop, clientHeight, scrollHeight } = document.documentElement
+  const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
 
-  return (scrollTop / (scrollHeight - clientHeight)) * 100
+  return (scrollTop / (scrollHeight - clientHeight)) * 100;
 }
