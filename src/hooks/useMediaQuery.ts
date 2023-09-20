67a4a0855892f23cfe65234503efa3fcde65d2f8
Cristian Figueredo <cristian28figueredo@gmail.com>
2023-09-19 20:04:08 -0400

    feat: useMediaQuery hook

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'preact/hooks'
+
+/**
+ * Reactive media query hook that returns the truthy value of the media query.
+ *
+ * @param {string} query
+ * @returns {boolean} boolean value of the query
+ */
+export function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false)
+
+  useEffect(() => {
+    setMatches(window.matchMedia(query).matches)
+  }, [])
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query)
+    const handler = (event: MediaQueryListEvent) => {
+      setMatches(event.matches)
+    }
+
+    mediaQuery.addEventListener('change', handler)
+
+    return () => {
+      mediaQuery.removeEventListener('change', handler)
+    }
+  }, [query])
+
+  return matches
+}
