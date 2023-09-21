ebfc7634e01f6e4324b7087536527e34dc2604b4
Cristian Figueredo <cristian28figueredo@gmail.com>
2023-09-20 22:04:28 -0400

    feat: BackgroundTitle with animation direction prop

--- src/components/background-title/index.tsx
+++ src/components/background-title/index.tsx
@@ -6,9 +6,13 @@ import { useIntersectionObserver } from '../../hooks/useIntersectionObserver'
 import { useMediaQuery } from '../../hooks/useMediaQuery'
 import { MEDIA_QUERY_BREAKPOINTS } from '../../constants/media-query'
 
-type TProps = PropsWithChildren<{ text: string }>
+type TProps = PropsWithChildren<{ text: string; direction?: 'left' | 'right' }>
 
-export const BackgroundTitle = ({ children, text }: TProps) => {
+export const BackgroundTitle = ({
+  children,
+  text,
+  direction = 'left',
+}: TProps) => {
   const title = useRef<HTMLHeadingElement | null>(null)
   const container = useRef<HTMLElement | null>(null)
   const offset = useRef(0)
@@ -17,22 +21,32 @@ export const BackgroundTitle = ({ children, text }: TProps) => {
   const { inView } = useIntersectionObserver(container)
 
   const isLargeSizeWindow = useMediaQuery(MEDIA_QUERY_BREAKPOINTS.LARGE)
+  const isXXSSizeWindow = useMediaQuery(MEDIA_QUERY_BREAKPOINTS.XXL)
 
   useEffect(() => {
-    if (isLargeSizeWindow) speed.current = 250
-  }, [isLargeSizeWindow])
+    if (isLargeSizeWindow) speed.current = 300
+    if (isXXSSizeWindow) speed.current = 500
+  }, [isLargeSizeWindow, isXXSSizeWindow])
 
   useScroll(({ scrollY }) => {
     if (!title.current || !inView) return
-    if (offset.current === 0) {
-      offset.current = scrollY
-    }
+    if (offset.current === 0) offset.current = scrollY
+
     const percentage = Math.round((offset.current - scrollY) * speed.current)
-    title.current.style.transform = `translate(${-percentage}%, 0)`
+    const translationX = direction === 'left' ? -percentage : percentage
+
+    title.current.style.transform = `translate(${translationX}%, 0)`
   })
 
   return (
-    <section ref={container} className={css.container}>
+    <section
+      ref={container}
+      className={css.container}
+      style={{
+        flexDirection: direction === 'left' ? 'row' : 'row-reverse',
+        paddingRight: direction === 'left' ? '0' : '50px',
+      }}
+    >
       <h3 ref={title} className={css.title}>
         {text}
       </h3>
