218788975fdc5a24bd6f119617a5f6c7173db0d6
Cristian Figueredo <cristian28figueredo@gmail.com>
2023-10-04 23:31:31 -0300

    feat: components/index.ts file & update related imports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './background-title'
+export * from './projects-section'
+export * from './screenshot'
+export * from './screenshot-group'
+export * from './screenshots'
+export * from './x-divider'
