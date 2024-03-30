52d2a35a48618f85f2a23dcd0e80eecedcd84347
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-30 15:58:43 -0400

    build: replace preact with react dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
 import vercel from '@astrojs/vercel/serverless';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()],
+  integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel()
 });
