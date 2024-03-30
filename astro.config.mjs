e40af181f90fa9476376fdfecff110e20691a775
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-30 15:18:15 -0400

    feat: tailwind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config'
-import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config';
+import preact from '@astrojs/preact';
+import vercel from '@astrojs/vercel/serverless';
 
-import vercel from '@astrojs/vercel/serverless'
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), tailwind()],
   output: 'server',
-  adapter: vercel(),
-})
+  adapter: vercel()
+});
