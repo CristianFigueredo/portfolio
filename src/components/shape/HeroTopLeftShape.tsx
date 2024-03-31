316195d2359b5d0a4cebbc4445d230cd857a1aa5
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-03-31 16:24:42 -0400

    feat(ui): hero svg's

---
+++ src/components/shape/HeroTopLeftShape.tsx
@@ -0,0 +1,18 @@
+export const HeroTopLeftShape = () => {
+    return (
+<svg className="absolute top-0 left-0" width="500" height="400" viewBox="0 0 598 493" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M563.594 287.468C424.594 555.61 72.5086 518.636 0 412.249V0H532.938C591 0 627.666 163.868 563.594 287.468Z" fill="url(#paint0_linear_2007_484)" fill-opacity="0.17" stroke="url(#paint1_radial_2007_484)"/>
+<defs>
+<linearGradient id="paint0_linear_2007_484" x1="0" y1="0" x2="593.022" y2="488.901" gradientUnits="userSpaceOnUse">
+<stop stop-color="white" stop-opacity="0"/>
+<stop offset="1" stop-color="white" stop-opacity="0.1"/>
+</linearGradient>
+<radialGradient id="paint1_radial_2007_484" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(570.723 276.465) rotate(-154.154) scale(634.159 177.066)">
+<stop stop-color="#F1C248" stop-opacity="0.24"/>
+<stop offset="1" stop-color="white" stop-opacity="0"/>
+</radialGradient>
+</defs>
+</svg>
+
+    )
+    }
---
+++ src/components/shape/index.ts
@@ -0,0 +1,2 @@
+export * from "./HeroTopLeftShape"
+export * from "./HeroBottomRightShape"
