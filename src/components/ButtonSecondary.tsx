5fdf1b23373bc4a533f5379bffd0d05b36ecaf0c
Cristian Figueredo <cristian28figueredo@gmail.com>
2024-04-20 12:07:16 -0400

    feat: implement send email button functionality in the footer

--- src/components/ButtonSecondary.tsx
+++ src/components/ButtonSecondary.tsx
@@ -1,8 +1,17 @@
 import { IconHandLoveYou } from '@tabler/icons-react'
+
+// TODO: decuple logic from UI component
+
+function sendEmail() {
+  const emailID = 'cristian25figueredo'
+
+  document.location = 'mailto:' + emailID + '@gmail.com'
+}
+
 export const ButtonSecondary = () => {
   return (
     <button
-      onClick={() => alert('This feature will be available soon.')}
+      onClick={sendEmail}
       className="px-4 py-2 rounded-md bg-white text-neutral-700 text-sm font-light hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200 flex items-center"
     >
       <IconHandLoveYou size={16} className="mr-2" />
